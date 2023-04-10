115e7d83811e119c4589c0ccac89543952fefe52
linkovervord <xlinkovervord@gmail.com>
2023-04-10 19:12:10 +0200

    Edited the build.bat so that it never skips the dm again when building

--- tools/build/build.bat
+++ tools/build/build.bat
@@ -1,2 +1,4 @@
 @echo off
+REM deleting the file tgstation.dmb automatically that way you don't have to do it every time that you start the build
+del tgstation.dmb
 "%~dp0\..\bootstrap\node.bat" --experimental-modules "%~dp0\build.js" %*
