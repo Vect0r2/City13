84861f819396b25fa3ea8fe7108c2818821a8040
linkovervord <xlinkovervord@gmail.com>
2023-04-10 20:32:59 +0200

    Fixed error message in terminal

--- tools/build/build.bat
+++ tools/build/build.bat
@@ -1,4 +1,6 @@
 @echo off
 REM deleting the file tgstation.dmb automatically that way you don't have to do it every time that you start the build
-del tgstation.dmb
+if exist tgstation.dmb (
+  del tgstation.dmb 2>nul
+)
 "%~dp0\..\bootstrap\node.bat" --experimental-modules "%~dp0\build.js" %*
